0d7fe79b6e4515f1c04c733509aa37e3a0814595
Yasuhisa Honda <nqvno14@gmail.com>
2025-08-31 21:33:46 +0900

    chore: modified project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,41 +7,38 @@
  */
 
 plugins {
-    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    alias(libs.plugins.kotlin.jvm)
+  // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
+  alias(libs.plugins.kotlin.jvm)
 
-    // Apply the application plugin to add support for building a CLI application in Java.
-    application
+  // Apply the application plugin to add support for building a CLI application in Java.
+  application
 }
 
 repositories {
-    // Use Maven Central for resolving dependencies.
-    mavenCentral()
+  // Use Maven Central for resolving dependencies.
+  mavenCentral()
 }
 
 dependencies {
-    // This dependency is used by the application.
-    implementation(libs.guava)
+  // This dependency is used by the application.
+  implementation(libs.guava)
 }
 
 testing {
-    suites {
-        // Configure the built-in test suite
-        val test by getting(JvmTestSuite::class) {
-            // Use Kotlin Test test framework
-            useKotlinTest("2.2.0")
+  suites {
+    // Configure the built-in test suite
+    val test by
+        getting(JvmTestSuite::class) {
+          // Use Kotlin Test test framework
+          useKotlinTest("2.2.0")
         }
-    }
+  }
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(24)
-    }
-}
+java { toolchain { languageVersion = JavaLanguageVersion.of(24) } }
 
 application {
-    // Define the main class for the application.
-    mainClass = "org.example.AppKt"
+  // Define the main class for the application.
+  mainClass = "co.hondaya.AppKt"
 }
